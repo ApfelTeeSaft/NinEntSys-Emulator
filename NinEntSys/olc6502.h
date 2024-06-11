5a67c03b0b915e490ffc7601539cc3093bd57a18
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-06-11 13:45:01 +0200

    CPU Instructions

--- NinEntSys/olc6502.h
+++ NinEntSys/olc6502.h
@@ -10,6 +10,19 @@ public:
 
 	void ConnectBus(Bus* n) { bus = n; }
 
+public:
+	enum FLAGS6502
+	{
+		C = (1 << 0), // Carry
+		Z = (1 << 1), // Zero
+		I = (1 << 2), // Disable Interrupts
+		D = (1 << 3), // Decimal
+		B = (1 << 4), // Break
+		U = (1 << 5), // Unused
+		V = (1 << 6), // Overflow
+		N = (1 << 7), // Negative
+	};
+
 private:
 	Bus* bus = nullptr;
 	uint8_t read(uint16_t a);
