a97ac5387900f8595b2a08709b7a5e7d36144eb8
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-06-11 09:50:22 +0200

    RW on Bus

--- NinEntSys/Bus.cpp
+++ NinEntSys/Bus.cpp
@@ -6,3 +6,15 @@ Bus::Bus()
 	// flush the toilet
 	for (auto& i : ram) i = 0x00;
 }
+
+Bus::~Bus()
+{
+}
+
+void Bus::write(uint16_t addr, uint8_t data)
+{
+}
+
+uint8_t Bus::read(uint16_t addr, bool bReadOnly)
+{
+}
