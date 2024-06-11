43a9002453f6d12a9b0e7aa60713181a60ee7b3b
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-06-11 09:53:28 +0200

    RAM Range Guard

--- NinEntSys/Bus.cpp
+++ NinEntSys/Bus.cpp
@@ -13,6 +13,9 @@ Bus::~Bus()
 
 void Bus::write(uint16_t addr, uint8_t data)
 {
+	// RAM Range Guard
+	if (addr >= 0x0000 && addr <= 0xFFFF)
+		ram[addr] = data;
 }
 
 uint8_t Bus::read(uint16_t addr, bool bReadOnly)
